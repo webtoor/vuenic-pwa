37049c442d14761af18497b05c9f29aefcfe7dd4
webtoor <fityanalimunshi@gmail.com>
2020-04-13 00:06:56 +0700

    SignIn Form + Validation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+   /*  redirectTo: 'folder/Inbox', */
+    redirectTo: 'signin',
     pathMatch: 'full'
   },
   {
--- src/app/pages/signin/signin.page.ts
+++ src/app/pages/signin/signin.page.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.page.html',
   styleUrls: ['./signin.page.scss'],
 })
-export class SigninPage implements OnInit {
 
-  constructor() { }
+export class SigninPage implements OnInit {
+  formLogin: FormGroup;
+  submitted = false;
+  constructor(public menu: MenuController, private formBuilder: FormBuilder) { 
+    this.menu.enable(false);
+  }
 
   ngOnInit() {
+    this.formLogin = this.formBuilder.group({
+      'email' : [null, [Validators.required, Validators.email]],
+      'password' : [null, Validators.required],
+    });
   }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.formLogin.invalid) {
+        return;
+    }
+    console.log(this.formLogin.value)
+
+  
+  }
+  get f() { return this.formLogin.controls; }
 
 }

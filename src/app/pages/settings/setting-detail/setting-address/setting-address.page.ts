30163334f12e66f901a8e0c256f23f8126f29e63
webtoor <fityanalimunshi@gmail.com>
2020-06-23 15:06:54 +0700

    Showing List Address Http Get

--- src/app/pages/settings/setting-detail/setting-address/setting-address.page.ts
+++ src/app/pages/settings/setting-detail/setting-address/setting-address.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-setting-address',
@@ -8,21 +9,33 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class SettingAddressPage implements OnInit {
   refreshPage = 0
-  constructor(public router : Router, public route : ActivatedRoute) { }
+  userAddress;
+  constructor(public router : Router, public route : ActivatedRoute, public httpService: AuthService) { }
 
   ngOnInit() {
+    this.getUserAddress()
     this.route.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
         this.refreshPage = parseInt(this.router.getCurrentNavigation().extras.state.refreshPage);
       }
       if(this.refreshPage == 1){
         console.log("Refresh Page")
+        this.getUserAddress()
       }
     });
   }
 
   ionViewDidLeave(){
     this.refreshPage = 0;
+  }
+
+  getUserAddress(){
+    this.httpService.GetRequest('user-address').subscribe(res => {
+        console.log(res);
+        if(res.status == 200){
+          this.userAddress = res.data
+        }
+      });
   }
 
   addMyAddress(){

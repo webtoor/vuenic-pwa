16182c86c7ede2ae030e8db158d8dfbe1248c789
webtoor <fityanalimunshi@gmail.com>
2020-07-13 14:29:37 +0700

    install chartjs + example

--- src/app/pages/chart/chart.page.ts
+++ src/app/pages/chart/chart.page.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef } from "@angular/core";
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.page.html',
   styleUrls: ['./chart.page.scss'],
 })
-export class ChartPage implements OnInit {
-
+export class ChartPage {
+  @ViewChild('barChart', {static: true}) barChart : ElementRef;
+  bars: any;
+  colorArray: any;
   constructor() { }
 
-  ngOnInit() {
+  ionViewDidEnter() {
+    this.createBarChart();
+  }
+
+  createBarChart() {
+    this.bars = new Chart(this.barChart.nativeElement, {
+      type: 'line',
+      data: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [
+              {
+                  label: 'My First dataset',
+                  fill: false,
+                  lineTension: 0.1,
+                  backgroundColor: 'rgba(75,192,192,0.4)',
+                  borderColor: 'rgba(75,192,192,1)',
+                  borderCapStyle: 'butt',
+                  borderDash: [],
+                  borderDashOffset: 0.0,
+                  borderJoinStyle: 'miter',
+                  pointBorderColor: 'rgba(75,192,192,1)',
+                  pointBackgroundColor: '#fff',
+                  pointBorderWidth: 1,
+                  pointHoverRadius: 5,
+                  pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                  pointHoverBorderColor: 'rgba(220,220,220,1)',
+                  pointHoverBorderWidth: 2,
+                  pointRadius: 1,
+                  pointHitRadius: 10,
+                  data: [65, 59, 80, 81, 56, 55, 40],
+                  spanGaps: false,
+              }
+          ]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
   }
 
 }

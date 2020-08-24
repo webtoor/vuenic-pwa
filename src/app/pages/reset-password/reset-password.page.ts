c11a15178327f65bbcc68c1e387765ec66125e0a
webtoor <fityanalimunshi@gmail.com>
2020-08-24 17:37:07 +0700

    setPassword reset-email

--- src/app/pages/reset-password/reset-password.page.ts
+++ src/app/pages/reset-password/reset-password.page.ts
@@ -11,10 +11,18 @@ import { ToastController } from '@ionic/angular';
 export class ResetPasswordPage implements OnInit {
   resetPWDForm: FormGroup;
   resetCodeForm: FormGroup;
+  setPasswordForm: FormGroup;
 
   submitted;
   resetPassword = true
   verifyEmail = false
+  setPassword = false
+  email;
+  showPassword = false;
+  passwordToggleIcon = "eye";
+  showConfirmPassword = false;
+  passwordConfirmToggleIcon = "eye";
+
   constructor(public toastController : ToastController, private formBuilder: FormBuilder, public httpService : AuthService) { }
 
   ngOnInit() {
@@ -25,6 +33,29 @@ export class ResetPasswordPage implements OnInit {
       'email' : [null, [Validators.required, Validators.email]],
       'code' : [null, [Validators.required]],
     });
+    this.setPasswordForm = this.formBuilder.group({
+      'email' : [null, [Validators.required, Validators.email]],
+      'password' : [null, [Validators.required]],
+      'confirmation_password' : [null, [Validators.required]],
+    });
+  }
+
+  togglePassword():void{
+    this.showPassword = !this.showPassword
+    if(this.passwordToggleIcon == "eye"){
+      this.passwordToggleIcon = "eye-off";
+    }else{
+      this.passwordToggleIcon = "eye";
+    }
+  }
+
+  toggleConfirmPassword():void{
+    this.showConfirmPassword = !this.showConfirmPassword
+    if(this.passwordConfirmToggleIcon == "eye"){
+      this.passwordConfirmToggleIcon = "eye-off";
+    }else{
+      this.passwordConfirmToggleIcon = "eye";
+    }
   }
 
   checkEmail() {
@@ -33,11 +64,15 @@ export class ResetPasswordPage implements OnInit {
         return;
     }
     console.log(this.resetPWDForm.value)
-    this.httpService.PostRequest(this.resetPWDForm.value, 'verify-code-reset').subscribe(res => {
+    this.httpService.PostRequest(this.resetPWDForm.value, 'reset-password').subscribe(res => {
       console.log(res)
       if(res.status == 200){
         this.resetPassword = false
         this.verifyEmail = true
+        this.email = res.email
+        this.resetCodeForm.patchValue({
+          'email' : res['email'],
+        }) 
 
       }else{
         this.presentToast(res.message)
@@ -47,6 +82,58 @@ export class ResetPasswordPage implements OnInit {
   }
   
   get f() { return this.resetPWDForm.controls; }
+
+  submitCheckCode() {
+    this.submitted = true;
+    if (this.resetCodeForm.invalid) {
+        return;
+    }
+    console.log(this.resetCodeForm.value)
+    this.httpService.PostRequest(this.resetCodeForm.value, 'verify-code-pwd').subscribe(res => {
+      console.log(res)
+      if(res.status == 200){
+        this.email = res.email  
+        this.setPasswordForm.patchValue({
+          'email' : res['email'],
+        }) 
+        this.resetPassword = false
+        this.verifyEmail = false
+        this.setPassword = true
+
+
+        
+      }else{
+        this.presentToast(res.message)
+      }
+  
+    });
+  }
+  
+  get g() { return this.resetCodeForm.controls; }
+
+  submitSetPassword() {
+    this.submitted = true;
+    if (this.setPasswordForm.invalid) {
+        return;
+    }
+    console.log(this.setPasswordForm.value)
+  /*   this.httpService.PostRequest(this.setPasswordForm.value, 'verify-code-pwd').subscribe(res => {
+      console.log(res)
+      if(res.status == 200){
+        this.email = res.email  
+        this.setPasswordForm.patchValue({
+          'email' : res['email'],
+        }) 
+        this.setPassword = true
+        
+      }else{
+        this.presentToast(res.message)
+      }
+  
+    }); */
+  }
+  
+  get h() { return this.resetCodeForm.controls; }
 
   async presentToast(msg) {
     const toast = await this.toastController.create({
